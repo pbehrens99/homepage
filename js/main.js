a89f8f4955f00b41011dad075be398c6f22b81b0
pbehrens99 <philipp.behrens@a-ov.de>
2021-01-07 10:56:40 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // jQuery run-up
+
 $(document).ready(function(){
 
 });
+
+
+
 
 // store variables
 /* "-" / latin letters aren't allowed in variable names. They also can't start with a digit */
@@ -19,3 +23,29 @@ message     = 'Hello'; // STRING
 
 let user = 'John', age = 25, message = 'Hello';  // more convenient+
 
+
+
+// fixed variables (constants)
+
+const name = 'Philipp', birthday = '08.01.1999';
+name = 'Phil' // wouldnt work because it's a constant
+
+/* EXAMPLE FOR USE */
+const COLOR_BLUE    = '#00F';
+const COLOR_RED     = '#F00';
+
+
+
+// embed variables
+
+let fullname = 'John Wayne';
+console.log(`The name is ${fullname}`); // ` ` instead of ' ' or " "
+
+
+
+// boolean
+let isGreater = 4 > 1;
+alert(isGreater) // true
+
+let isSmaller = 4 < 1;
+alert(isSmaller); // false
