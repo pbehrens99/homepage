eeaf211c52c661f6da2d71729d53bee4b807e36c
pbehrens99 <philipp.behrens@a-ov.de>
2021-01-07 10:31:47 +0100

    CSS+JS

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ if (isset($_GET['page'])) {
 <html>
     <head>
         <title><?php echo $Controller->getPageTitle($page); ?></title>
+        <script src='https://code.jquery.com/jquery-3.5.1.js'></script>
+        <script src='js/main.js'></script>
+        <link rel='stylesheet' type='text/css' href='css/main.css'/>
         <meta charset="UTF-8"/>
     </head>
     <body>
