6d83a8cc4bdf5b2abd1174c8fb0f1107516ddf27
pbehrens99 <philipp.behrens@a-ov.de>
2021-01-07 11:10:02 +0100

    JS

--- js/main.js
+++ js/main.js
@@ -49,3 +49,62 @@ alert(isGreater) // true
 
 let isSmaller = 4 < 1;
 alert(isSmaller); // false
+
+alert( 2 > 1 );  // true (correct)
+alert( 2 == 1 ); // false (wrong)
+alert( 2 != 1 ); // true (correct)
+
+alert( 'Z' > 'A' ); // true
+alert( 'Glow' > 'Glee' ); // true
+alert( 'Bee' > 'Be' ); // true
+
+// boolean conversion
+
+alert(Boolean(1)); // == true (1 = true)
+alert(Boolean(" ")); // == true (" " = true)
+alert(Boolean(0)); // == false (0 = false)
+alert(Boolean("")); // == false ("" = false)
+
+
+//typeof 
+
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function" (3)
+
+
+
+// maths
+
+// add
+alert(5+5); // 10
+
+// sub
+alert(5-5); // 0;
+
+// multiply
+alert(5*5); // 25
+
+// divide
+alert(5/5); // 1
+
+// exponentiation (hochzahl)
+alert(5**3); // 5^3 bzw. 5*5*5 == 125
+
+// remainder (Rest)
+alert (5%2); // == 1 (2*2 = 4 + 1 = 5)
+
