1fb9208dd3d74905182b6cad21f40b1f8b291811
pbehrens99 <philipp.behrens@a-ov.de>
2021-01-07 10:44:33 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// jQuery run-up
 $(document).ready(function(){
-    
+
 });
+
+// store variables
+/* "-" / latin letters aren't allowed in variable names. They also can't start with a digit */
+
+let message; // new version
+message     = 'Content';
+
+var message2; // old version
+message2    = 'Content';
+
+let user, age, message;
+user        = 'John'; // STRING
+age         = 25; // INT
+message     = 'Hello'; // STRING
+
+let user = 'John', age = 25, message = 'Hello';  // more convenient+
+
